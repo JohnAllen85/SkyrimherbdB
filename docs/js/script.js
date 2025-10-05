623202eb944a3d0ac7e66d77591647c837cd3587
Jonathan Allen <JohnAllen85@users.noreply.github.com>
2025-10-05 14:29:12 -0700

    Update script.js
    Whoops, replaced with a css file. Re-uploaded

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,190 +1,51 @@
-body {
-    background-color: #96B6C5;
-	font: 16px arial, sans-serif;
-	color: #000000;
-}
-
-caption {
-	font: 24px arial, sans-serif;
-	color: #000000;
-	font-weight: bold;
-}
-
-
-div.data {
-	margin-left: 110px;
-	max-width:calc(100vw - 160px);
-	margin-right:50px;
-	z-index = 1;
-}
-
-a {
-	color: #34383a;
-}
-
-table {
-	border-collapse: collapse;
-	background-color: #c9d1d8;
-}
-
-td {
-	border: 1px solid #61686c;
-	padding: 5px;
-	background-color: #F1F0E8;
-	color: #34383a;
-}
-
-th {
-	border: 1px solid #61686c;
-	padding: 5px;
-	background-color: #F1F0E8;
-	color: #34383a;
-}
-
-td.title{
-	font-weight: bold;
-	height:40px;
-}
-
-td.link{
-	height:40px;
-}
-
-td:hover {
-	background-color: #c9d1d8;
-	color: #12100E;
-	cursor: pointer;
-}
-
-div.pagemenu {
-	position: fixed;
-	top: 0px;
-	width: 100px;
-	left: 0px;
-	z-index = 10;
-}
-
-table.slidemenu {
-	position:fixed;
-	top:100px;
-	width: 110px;
-	left: 0px;
-	box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.5);
-}
-
-table.hidden {
-	position: fixed;
-	left: -250px;
-	box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.5);
-}
-
-th.limited {
-	min-width:100px;
-	max-width:200px;
-}
-
-th.byNameLimited {
-	min-width:100px;
-	max-width:250px;
-}
-
-th.byEffectLimited {
-	min-width:250px;
-	max-width:500px;
-}
-
-th.healingLimited {
-	width:12.5%;
-	max-width: 150px;
-}
-
-th#healingDescription {
-	width: 25%;
-}
-
-th.enhancementLimited {
-	width:16.66%;
-	max-width: 150px;
-}
-
-th#enhancementDescription {
-	width: 33.33%;
-}
-
-th.spellResistsLimited {
-	width:11.11%;
-	max-width: 150px;
-}
-
-th#spellResistsDescription {
-	width: 22.22%;
-}
-
-th.poisonsLimited {
-	width:11.11%;
-	max-width: 150px;
-}
-
-th#poisonsDescription {
-	width: 22.22%;
-}
-
-th.utilityLimited {
-	width:12.5%;
-	max-width: 150px;
-}
-
-th#utilityDescription {
-	width: 25%;
-}
-	
-th.otherLimited {
-	width:11.11%;
-	max-width: 150px;
-}
-
-th#otherDescription {
-	width: 22.22%;
-}
-
-td.title {
-	font-weight: bold;
-}
-
-tr.menus {
-	height:30px;
-}
-
-table#hiddenHerb1 {
-	position: fixed;
-	top: 105px;
-}
-
-table#hiddenHerb2 {
-	position: fixed;
-	top: 135px;
-}
-
-table#hiddenHerb3 {
-	position: fixed;
-	top: 165px;
-}
-
-table#tableByName {
-	min-width: 500px;
-	max-width: 100%;
-}
-
-table#tableByEffect {
-	min-width: 500px;
-	max-width: 1000px;
-}
-
-table.Recipes {
-	width:100%;
-}
-
-h2 {
-	font: 32px arial, sans-serif;
-	font-weight: bold;
-}
+    function showHerbMenu(whichmenu, behavior){
+	if (whichmenu == 0){
+		if (behavior == 0){
+			document.getElementById('hiddenHerb1').style.left="-250px";
+			document.getElementById('hiddenHerb2').style.left="-250px";
+			document.getElementById('hiddenHerb3').style.left="-250px";
+		}
+	}
+    else if (whichmenu == 1) {
+          var ele=document.getElementById('hiddenHerb1');
+          var left = ele.offsetLeft;
+          if (left == 100 && (behavior == 0 || behavior == 2)){
+              ele.style.left="-250px";
+            }
+          else if (behavior == 1 || behavior == 2){
+              ele.style.left="100px";
+              document.getElementById('hiddenHerb2').style.left="-250px";
+              document.getElementById('hiddenHerb3').style.left="-250px";
+           }
+       }
+    else if (whichmenu == 2) {
+          var ele=document.getElementById('hiddenHerb2');
+          var left = ele.offsetLeft;
+          if (left == 100 && (behavior == 0 || behavior == 2)){
+              ele.style.left="-250px";
+            }
+          else if (behavior == 1 || behavior == 2){
+              ele.style.left="100px";
+              document.getElementById('hiddenHerb1').style.left="-250px";
+              document.getElementById('hiddenHerb3').style.left="-250px";
+           }
+      }
+     else if (whichmenu == 3) {
+          var ele=document.getElementById('hiddenHerb3');
+          var left = ele.offsetLeft;
+          if (left == 100 && (behavior == 0 || behavior == 2)){
+              ele.style.left="-250px";
+            }
+          else if (behavior == 1 || behavior == 2){
+              ele.style.left="100px";
+              document.getElementById('hiddenHerb1').style.left="-250px";
+              document.getElementById('hiddenHerb2').style.left="-250px";
+           }
+      }
+      else {
+          document.getElementById('hiddenHerb1').style.left="-250px";
+          document.getElementById('hiddenHerb2').style.left="-250px";
+          document.getElementById('hiddenHerb3').style.left="-250px";
+      }
+       return false;
+    }
